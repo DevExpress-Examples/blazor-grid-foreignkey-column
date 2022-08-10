c55a40d883156325a7bc112e8df7d521403f9054
Elena Khamlyuk <elena.khamlyuk@devexpress.com>
2022-08-10 15:52:03 +0400

    add the project folder

---
+++ CS/GridForeignColumn/Program.cs
@@ -0,0 +1,45 @@
+using GridForeignColumn.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using GridForeignColumn.Data.Northwind;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddDevExpressBlazor();
+builder.Services.AddEntityFrameworkSqlServer();
+builder.Services.AddDbContextFactory<NorthwindContext>((sp, options) => {
+    var env = sp.GetRequiredService<IWebHostEnvironment>();
+    var dbPath = Path.Combine(env.ContentRootPath, "NWind.mdf");
+    options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Integrated Security=true;AttachDbFileName=" + dbPath);
+});
+builder.Services.Configure<DevExpress.Blazor.Configuration.GlobalOptions>(options => {
+    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+});
+builder.WebHost.UseWebRoot("wwwroot");
+builder.WebHost.UseStaticWebAssets();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
